2892dc58b2c372eb81df05f578be0e13afeef081
mang <ningma003@gmail.com>
2013-12-10 23:38:03 +0800

    fix 删除无用脚本 同时增加了一个挂载windows的脚本

---
+++ mount_windows.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+WINDOWS_F=/media/mang/windows/f
+
+# 如果目录不存在就先创建目录
+if [ ! -d $WINDOWS_F ];then
+   mkdir -p $WINDOWS_F
+fi
+
+mount /dev/sda7  $WINDOWS_F &>/dev/null
---
+++ startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+site=https://i.doitim.com/home/#/time/today
+
+# 开机启动的网址
+firefox $site &>/dev/null &
